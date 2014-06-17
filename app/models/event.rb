bf33bdfbfc2d5f25cc95205e5f0b6e3ecf439209
fly1tkg <fly1tkg@gmail.com>
2014-06-17 20:50:52 +0900

    開始時間と終了時間を入れる

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,15 +5,17 @@ class Event < ActiveRecord::Base
   enumerize :kind, in: [:study, :drink]
 
   def self.create_or_update_by_zusaar(zusaar_id, kind, price)
-    result = Zusaar.search_users(event_id: zusaar_id)
 
-    return nil unless zusaar_event = result.events[0]
+    return nil unless zusaar_event = Zusaar.search_users(event_id: zusaar_id).events[0]
+    return nil unless zusaar_users = zusaar_event.users
+
+    return nil unless zusaar_event = Zusaar.search_events(event_id: zusaar_id).events[0]
 
     users_id = []
 
     ActiveRecord::Base.transaction do
       # ユーザを作成か更新
-      zusaar_event.users.each do |zusaar_user|
+      zusaar_users.each do |zusaar_user|
         user = User.find_by(zusaar_id: zusaar_user.id) || User.new(zusaar_id: zusaar_user.id)
         user.name = zusaar_user.nickname
         user.save!
@@ -25,7 +27,8 @@ class Event < ActiveRecord::Base
       # eventを作成か更新
       event = find_by(zusaar_id: zusaar_id) || new(zusaar_id: zusaar_id)
       event.attributes = {
-          register_users: users_id, kind: kind, price: price, title: zusaar_event.title
+          register_users: users_id, kind: kind, price: price, title: zusaar_event.title,
+          started_at: zusaar_event.started_at, ended_at: zusaar_event.ended_at
       }
       event.save!
     end
