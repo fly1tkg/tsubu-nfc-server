52deaa036e680a778963b1e9125be0ba5a4e09ab
fly1tkg <fly1tkg@gmail.com>
2014-06-17 20:20:21 +0900

    Eventモデルをリファクタリング

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,29 +7,25 @@ class Event < ActiveRecord::Base
   def self.create_or_update_by_zusaar(zusaar_id, kind, price)
     result = Zusaar.search_users(event_id: zusaar_id)
 
-    if zusaar_event = result.events[0]
-      users_id = []
+    return nil unless zusaar_event = result.events[0]
 
-      # ユーザを作成か更新
-      zusaar_event.users.each do |zusaar_user|
-        if user = User.find_by(zusaar_id: zusaar_user.id)
-          user.update(name: user.name)
-        else
-          user = User.create(zusaar_id: zusaar_user.id, name: zusaar_user.nickname)
-        end
+    users_id = []
 
-        # 参加ユーザのidをpush
-        users_id << user.id
-      end
+    # ユーザを作成か更新
+    zusaar_event.users.each do |zusaar_user|
+      user = User.find_by(zusaar_id: zusaar_user.id) || User.new(zusaar_id: zusaar_user.id)
+      user.name = zusaar_user.nickname
+      user.save!
 
-      # eventを作成か更新
-      if event = find_by(zusaar_id: zusaar_id)
-        event.update(register_users: users_id, kind: kind, price: price, title: zusaar_event.title)
-      else
-        create(zusaar_id: zusaar_id, register_users: users_id, kind: kind, price: price, title: zusaar_event.title)
-      end
-    else
-      return nil
+      # 参加ユーザのidをpush
+      users_id << user.id
     end
+
+    # eventを作成か更新
+    event = find_by(zusaar_id: zusaar_id) || new(zusaar_id: zusaar_id)
+    event.attributes = {
+        register_users: users_id, kind: kind, price: price, title: zusaar_event.title
+    }
+    event.save!
   end
 end
